ce7ccf37e21a71ee123215b55a31b557864f8258
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-12-05 17:24:40 -0300

    feat(src): organizando a estrutura de pasta do projeto, e criando o sistema de rotas
    Criado os arquivos @types, layout, pages, home, styles, theme e o Router.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { defaultTheme } from './styles/theme/default'
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from 'react-router-dom'
+import { DefaultLayoutContainer } from './styles'
+
+export function DefaultLayout() {
+  return (
+    <DefaultLayoutContainer>
+      <Outlet />
+    </DefaultLayoutContainer>
+  )
+}
